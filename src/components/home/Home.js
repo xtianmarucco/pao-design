5839b78ca54df32afb3ee54b54d57698ca595988
xtianmarucco <christian.marucco@gmail.com>
2024-05-14 02:03:01 -0300

    initial settings

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.scss'; // Importa los estilos Sass de la sección Home
+
+const Home = () => {
+    return (
+        <div className="home-section">
+            <h1>hola soy el home</h1>
+        </div>
+    );
+}
+
+export default Home;
