aa4cd3e48105f5b0222295966bdf65600fd05a30
xtianmarucco <christian.marucco@gmail.com>
2024-05-29 01:27:02 -0300

    style fixes on about me section

--- src/components/about-me-section/AboutMe.js
+++ src/components/about-me-section/AboutMe.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./about-me.scss";
+import "./about-me.css";
 import SectionTitle from "../section-title/SectionTitle";
 import TextBox from "../text-box/TextBox";
 import content from "../../data/content.json";
@@ -7,28 +7,41 @@ import Image from "../image/Image";
 
 const AboutMe = () => {
   return (
-    <div id="about-me-section" className="about-me-section">
-      <SectionTitle
-        title="About me"
-        classNameImg="about-me-icon"
-        imgSrc="/about-me-icon.svg"
-        classNameTitle="about-me-title"
-        borderWidth={3}
-        borderColor="black"
-        borderRadius="35px"
-        cornerSize="16px"
-        backgroundColor="#4ACFFF"
-     
-      />
-    
-      <div className="parent">
-      <Image
-          className="about-photo"
-          src="/about-photo.svg"
-          alt="welcome logo"
-        />
-        <Image className="about-personal" src="/about-personal.png" />
-        <TextBox text={content.aboutMeText} />
+    <div id="about-me-section">
+      <div className="about-me-container">
+        <div className="title-section-container">
+          <SectionTitle
+            title="About me"
+            classNameImg="about-me-icon"
+            imgSrc="/about-me-icon.svg"
+            classNameTitle="about-me-title"
+            borderWidth={3}
+            borderColor="black"
+            borderRadius="35px"
+            cornerSize="16px"
+            backgroundColor="#4ACFFF"
+          />
+        </div>
+        <div className="about-photo-container">
+          <Image
+            className="about-photo"
+            src="/about-photo.svg"
+            alt="welcome logo"
+          />
+        </div>
+        <div className="about-personal-container">
+          <Image className="about-personal" src="/about-personal.png" />
+        </div>
+        <div className="text-box-container">
+          <TextBox text={content.aboutMeText} />
+        </div>
+        <div className="about-personal-mobile-container">
+          <Image
+            className="about-personal-mobile"
+            src="/about-personal-mobile.svg"
+          />
+        </div>
+       
       </div>
     </div>
   );
